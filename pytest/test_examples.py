63acecbff178aa038df0cdcc45c41ea5ba9897bb
Jacob Alexander <haata@kiibohd.com>
2019-10-13 15:52:53 -0700

    More fixes for pytest GitHub Actions
    - cython needs to be available for pycapnp to build
    - Cleaned up pytest test to wait for socket port rather than just
    sleeping a fixed amount of time

--- pytest/test_examples.py
+++ pytest/test_examples.py
@@ -1,17 +1,36 @@
 import os
+import socket
 import subprocess
+import sys
 import time
 
 examples_dir = os.path.join(os.path.dirname(__file__))
 
 def run_subprocesses(address, client):
     server = subprocess.Popen(['cargo', 'run', 'server', address], cwd=os.path.join(examples_dir, '..'))
-    time.sleep(5)  # Give the server some small amount of time to start listening
-    client = subprocess.Popen([os.path.join(examples_dir, client), address])
+
+    # Wait for server to start
+    addr, port = address.split(':')
+    retries = 20
+    while True:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((addr, int(port)))
+        if result == 0:
+            break
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        result = sock.connect_ex((addr, int(port)))
+        if result == 0:
+            break
+        # Give the server some small amount of time to start listening
+        time.sleep(1)
+        retries -= 1
+        if retries == 0:
+            assert False, "Timed out waiting for server to start"
+    client = subprocess.Popen([sys.executable, os.path.join(examples_dir, client), address])
 
     ret = client.wait()
     server.kill()
-    assert ret == 0
+    assert ret == 0, "Client did not return 0"
 
 
 def test_ssl_async_example():
